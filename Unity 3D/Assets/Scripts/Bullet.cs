f089a5d59fe732e42c4077312d339044f5fb4037
Chris.Suo <SuoChenHua@gmail.com>
2021-01-16 16:16:14 +0800

    add Tower
    add tower

---
+++ Unity 3D/Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+    private void Update()
+    {
+        Track();
+    }
+
+    private void Track()
+    {
+        Vector3 posA = target.position;
+        Vector3 posB = transform.position;
+        transform.position = Vector3.Lerp(posA, posB, Time.deltaTime * 0.5f * speed);
+
+    }
+}
---
+++ Unity 3D/Assets/Scripts/Tower.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public float attackRange = 15;
+    public float attackPower = 20;
+    public GameObject bullet;
+    public float speed = 100;
+    public int layer;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawSphere(transform.position, attackRange);
+    }
+
+    private void Update()
+    {
+        Track();
+    }
+
+    private void Track()
+    {
+        Collider[] hit = Physics.OverlapSphere(transform.position, attackRange, 1 << layer);
+
+        if (hit.Length > 0)
+        {
+            GameObject temp = Instantiate(bullet, transform.position + transform.up * 10, Quaternion.identity);
+            Bullet bulletIns = temp.AddComponent<Bullet>();
+            bulletIns.target = hit[0].transform;
+            bulletIns.speed = speed;
+
+        }
+    }
+}
